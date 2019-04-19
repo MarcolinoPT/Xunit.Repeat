3069c132f472e580020dd7d98fbd0a55b9db784d
Marco de Freitas <marcogfreitas@gmail.com>
2019-04-19 18:01:13 +0100

    Add integration tests
    Also renamed the namespaces.

---
+++ xUnit.Repeat.net.Tests/IntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿namespace xUnit.Repeat.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+    using Xunit.Priority;
+    using Xunit.Repeat;
+
+    [TestCaseOrderer(ordererTypeName: PriorityOrderer.Name, ordererAssemblyName: PriorityOrderer.Assembly)]
+    public class IntegrationTests
+    {
+        private const int RepeatCount = 10;
+        private static int Counter;
+
+        [Theory(DisplayName = "It should repeat the test"), Priority(1)]
+        [Repeat(RepeatCount)]
+        public void It_should_repeat_the_test(int interationNumber)
+        {
+            ++Counter;
+        }
+
+        [Fact(DisplayName = "It should have repeated the test"), Priority(2)]
+        public void It_should_have_repeated_the_test()
+        {
+            Counter
+                .Should()
+                .Be(RepeatCount);
+        }
+    }
+}
